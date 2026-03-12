1de1f79ed0b572bd15f0dfbc7806490ca391488a
Jeremy Mercado <jeremy.j.mercado@hotmail.com>
2026-03-12 23:23:38 +0100

    Add: plugin for pwa

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,34 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Meine Rezept App',
+        short_name: 'Rezepte',
+        description: 'Offline Kochbuch für den Raspberry Pi',
+        theme_color: '#f97316', // Das Orange von Tailwind
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
   ],
   resolve: {
     alias: {
